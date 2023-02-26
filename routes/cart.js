5a9c7592bd20f1a00976a7299615f0df015f1a69
CemAkan <cemakan5014@gmail.com>
2023-02-26 05:07:30 +0300

    syntax error solved

--- routes/cart.js
+++ routes/cart.js
@@ -4,10 +4,10 @@ var router = express.Router();
 //--> routes for cart <--
 
 //--> list all items that were added to cart list<--
-router.get("/cart", function (req, res, next) {});
+router.get("/list", function (req, res, next) {});
 //--> get total price of all items that were added to cart list<--
 router.get("/price", function (req, res, next) {});
 //--> add items to cart list<--
-router.post("/cart", function (req, res, next) {});
+router.post("/add", function (req, res, next) {});
 
 module.exports = router;
