cc62fd2970a08560c3d66d72c75d9f0a15cee2dc
CemAkan <cemakan5014@gmail.com>
2023-02-26 04:13:31 +0300

    welcome message was added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {});
+router.get("/", function (req, res, next) {
+  res.send(
+    "Welcome to my api, for usage of my api you can read to documentation of my api."
+  );
+});
 
 module.exports = router;
