1e1fd1035a33f56aefe85f1107f0ae7dc8117041
CemAkan <cemakan5014@gmail.com>
2023-02-26 04:09:22 +0300

    cart route was added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var usersRouter = require("./routes/items");
 var usersRouter = require("./routes/like");
+var usersRouter = require("./routes/cart");
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/items", usersRouter);
 app.use("/like", usersRouter);
+app.use("/cart", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
