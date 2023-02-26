b01b98da2cb4cc90d9b2f5a04fcdbfa725e8bf79
CemAkan <cemakan5014@gmail.com>
2023-02-26 19:30:58 +0300

    little syntax edits

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var itemRouter = require("./routes/items");
 var likeRouter = require("./routes/like");
-var cartRouter = require("./routes/cart");
+var cartRouter = require("./routes/cart.js");
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/items", itemRouter);
 app.use("/like", likeRouter);
-app.use("/cart", cartRouter);
+app.use("/carts", cartRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
