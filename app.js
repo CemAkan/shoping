abd3d9d55f6d1d05879f157b9973d0e3975784c9
CemAkan <cemakan5014@gmail.com>
2023-02-26 04:06:32 +0300

    like route was added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var usersRouter = require("./routes/items");
+var usersRouter = require("./routes/like");
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/items", usersRouter);
+app.use("/like", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
