eaf398abd38178d0464d3d40d09800e6ad7a2c51
CemAkan <cemakan5014@gmail.com>
2023-02-28 02:49:01 +0300

    syntax error in requirements solved

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/user/users");
 var itemRouter = require("./routes/admin/items");
 var likeRouter = require("./routes/user/like");
-var cartRouter = require("./routes/user");
+var cartRouter = require("./routes/user/users");
 var categoryRouter = require("./routes/admin/category");
 
 var app = express();
