45200fc656f2acbfb743a3d3db0deb27efd3273a
CemAkan <cemakan5014@gmail.com>
2023-02-27 19:36:15 +0300

    primary key was edited

--- models/category/category.js
+++ models/category/category.js
@@ -1,11 +1,12 @@
 module.exports = (sequelize, Sequelize) => {
   return sequelize.define("category", {
-    id: {
+    categoryId: {
       type: Sequelize.INTEGER,
       primaryKey: true,
       readOnly: true,
       allowNUll: false,
       unique: true,
+      autoIncrement: true,
     },
     name: {
       type: Sequelize.STRING,
