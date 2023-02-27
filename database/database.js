a32adb4ff6ebae96a92c58dc4d74ab9748b85f16
CemAkan <cemakan5014@gmail.com>
2023-02-28 01:37:04 +0300

    requirements were added

---
+++ database/database.js
@@ -0,0 +1,7 @@
+//--> Requirements <--
+var user = require("../models/user/user");
+var cart = require("../models/cart/cart");
+var category = require("../models/category/category");
+var like = require("../models/likeList/likeList");
+var item = require("../models/items/item");
+var Database = require("../config/config");
