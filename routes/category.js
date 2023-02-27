1796f44f8a173e82a42a36de1baac4fc61486d4e
CemAkan <cemakan5014@gmail.com>
2023-02-27 15:53:22 +0300

    template methods of categories

---
+++ routes/category.js
@@ -0,0 +1,21 @@
+//--> Module dependencies.<--
+var express = require("express");
+var app = express();
+var router = express.Router();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+var _ = require("underscore");
+var db = require("../connection");
+
+//--> associations <--
+
+//add
+
+//update
+
+//delete
+
+//list all
+
+//exporting
+module.exports = router;
