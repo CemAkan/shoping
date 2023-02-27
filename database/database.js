6dfe797f84f906d94ad006f2344317804e9639ab
CemAkan <cemakan5014@gmail.com>
2023-02-28 01:37:48 +0300

    database connection were added

--- database/database.js
+++ database/database.js
@@ -5,3 +5,15 @@ var category = require("../models/category/category");
 var like = require("../models/likeList/likeList");
 var item = require("../models/items/item");
 var Database = require("../config/config");
+
+//--> Database Connection <--
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  Database.development.database,
+  Database.development.username,
+  Database.development.password,
+  {
+    host: Database.development.host,
+    dialect: Database.development.dialect,
+  }
+);
