d876bbd067591031052043f5ac2c60a0fecb3a1a
CemAkan <cemakan5014@gmail.com>
2023-02-28 02:21:34 +0300

    middleware for check token

---
+++ middleware/middleware.js
@@ -0,0 +1,29 @@
+const database = require("../database/database");
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization
+    ? req.headers.authorization.split(" ")[1]
+    : null;
+  if (token != null) {
+    database.userModel
+      .findOne({
+        where: {
+          access_token: token,
+        },
+      })
+      .then((result) => {
+        if (result != null) {
+          req.body.userData = result.toJSON();
+          next();
+        } else {
+          return res.status(401).send({
+            message: "UnAuthenticated",
+          });
+        }
+      });
+  } else {
+    return res.status(401).send({
+      message: "UnAuthenticated",
+    });
+  }
+};
