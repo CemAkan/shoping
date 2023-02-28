b2551b049e4abcd18d1b51aed5041a0ccd4d539b
CemAkan <cemakan5014@gmail.com>
2023-02-28 09:03:41 +0300

    syntax error solved and sucessfully linked

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -6,10 +6,8 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 var _ = require("underscore");
 var { userModel } = require("../database/database");
-const checkAuth = require("../middleware/middleware");
 const crypter = require("../services/passwordCrypt");
 const model = require("../services/modelService");
-const userValidator = require("../validators/authValidator");
 
 //--> METHODS FOR /user <--
 
@@ -17,11 +15,9 @@ const userValidator = require("../validators/authValidator");
 var users = {};
 
 //--> list all user <--
-users.list =
-  (checkAuth,
-  async (req, res, next) => {
-    res.json(await model.findAll(userModel));
-  });
+users.list = async (req, res, next) => {
+  res.json(await model.findAll(userModel));
+};
 
 //--> login <--
 users.signIn = async (req, res, next) => {
@@ -49,73 +45,69 @@ users.signIn = async (req, res, next) => {
 };
 
 //--> add a new user <--
-users.signUp =
-  (userValidator.signUp,
-  (req, res) => {
-    let body = req.body;
-    var hash = crypter(body.password);
-    body.password = hash;
+users.signUp = (req, res) => {
+  let body = req.body;
+  var hash = crypter(body.password);
+  body.password = hash;
 
-    model.create(userModel, body).then(
-      (resign) => {
-        res.json(resign);
-      },
-      (err) => {
-        res.status(400).send({
-          error: "Please use correct writing rules.",
-        });
-      }
-    );
-  });
+  model.create(userModel, body).then(
+    (resign) => {
+      res.json(resign);
+    },
+    (err) => {
+      res.status(400).send({
+        error: "Please use correct writing rules.",
+      });
+    }
+  );
+};
 
 //--> update a user <--
-users.update =
-  (userValidator.updateUser,
-  (req, res) => {
-    let personId = req.params.id;
-    let body = _.pick(req.body, "username", "email", "password");
-    let attributes = {};
+users.update = (req, res) => {
+  let personId = req.params.id;
+  let body = _.pick(req.body, "username", "email", "password");
+  let attributes = {};
 
-    if (body.hasOwnProperty("username")) {
-      attributes.username = body.username;
-    }
+  if (body.hasOwnProperty("username")) {
+    attributes.username = body.username;
+  }
 
-    if (body.hasOwnProperty("email")) {
-      attributes.email = body.email;
-    }
+  if (body.hasOwnProperty("email")) {
+    attributes.email = body.email;
+  }
 
-    if (body.hasOwnProperty("password")) {
-      attributes.password = body.password;
-    }
+  if (body.hasOwnProperty("password")) {
+    attributes.password = body.password;
+  }
 
-    model
-      .findOne(userModel, {
-        where: {
-          id: personId,
-        },
-      })
-      .then(
-        (resign) => {
-          if (resign) {
-            resign.update(attributes).then(
-              (resign) => {
-                res.json(resign);
-              },
-              () => {
-                res.status(400).send();
-              }
-            );
-          } else {
-            res.status(404).send({
-              error: "Person can not found.",
-            });
-          }
-        },
-        () => {
-          res.status(500).send();
+  model
+    .findOne(userModel, {
+      where: {
+        id: personId,
+      },
+    })
+    .then(
+      (resign) => {
+        if (resign) {
+          resign.update(attributes).then(
+            (resign) => {
+              res.json(resign);
+            },
+            () => {
+              res.status(400).send();
+            }
+          );
+        } else {
+          res.status(404).send({
+            error: "Person can not found.",
+          });
         }
-      );
-  });
+      },
+      () => {
+        res.status(500).send();
+      }
+    );
+};
 
 //--> delete a user <--
 users.delete = (req, res, next) => {
--- routes/user/users.js
+++ routes/user/users.js
@@ -3,20 +3,22 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var control = require("../../controllers/userControl");
+const checkAuth = require("../../middleware/middleware");
+const userValidator = require("../../validators/authValidator");
 
 //--> METHODS FOR /user <--
 
 //--> list all user <--
-router.get("/list", control.list);
+router.get("/list", checkAuth, control.list);
 
 //--> login <--
 router.post("/sign-in", control.signIn);
 
 //--> add a new user <--
-router.post("/sign-up", control.signUp);
+router.post("/sign-up", userValidator.signUp, control.signUp);
 
 //--> update a user <--
-router.put("/update/:id", control.update);
+router.put("/update/:id", userValidator.updateUser, control.update);
 
 //--> delete a user <--
 router.delete("/delete/:id", control.delete);
