6211eb618a37253aa779eb3efc858a7c8b72c3fb
CemAkan <cemakan5014@gmail.com>
2023-02-28 07:22:08 +0300

    password validation and type changed

--- models/user/user.js
+++ models/user/user.js
@@ -26,7 +26,7 @@ module.exports = (sequelize, Sequelize) => {
       },
     },
     password: {
-      type: DataTypes.STRING(64),
+      type: Sequelize.STRING(64),
       validate: {
         is: /^[0-9a-f]{64}$/i,
       },
