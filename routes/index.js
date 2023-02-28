314d222395a415b900e3567ce4e05f1a8f5430db
CemAkan <cemakan5014@gmail.com>
2023-02-28 04:36:40 +0300

    old type function convert to arrow function

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res, next) => {
   res.send(
-    "Welcome to my api, for usage of my api you can read to documentation of my api."
+    "Welcome to first version of shoping api, for usage of my api you can read to documentation."
   );
 });
 
