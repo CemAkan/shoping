a3f2666e46f41485bec1e7bb0ebf1b9fac16d563
CemAkan <cemakan5014@gmail.com>
2023-03-01 18:16:02 +0300

    try-catch added and then convert to aync await

--- controllers/cartControler.js
+++ controllers/cartControler.js
@@ -10,157 +10,152 @@ const model = require("../services/modelService");
 
 module.exports = {
   //--> list all items that were added to cart <--
-  list: (req, res, next) => {
-    let personId = req.params.id;
-    model
-      .findAll(cartModel, {
+  list: async (req, res, next) => {
+    try {
+      let personId = req.params.id;
+      var listModel = await model.findAll(cartModel, {
         where: {
           customerId: personId,
         },
-      })
-      .then(
-        (cart) => {
-          let a = 0;
-          let listofCart = [];
-          for (i in cart) {
-            listofCart.push(cart[a].itemIds);
-            a = a + 1;
-          }
-          res.send(listofCart);
-        },
-        () => {
-          res.status(400).send();
-        }
-      );
+      });
+
+      res.json({
+        status: "success",
+        data: listModel,
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: error,
+      });
+    }
   },
   //--> get total price of all items that were added to cart <--
-  price: (req, res, next) => {
-    let personId = req.params.id;
-    model
-      .findAll(cartModel, {
+  price: async (req, res, next) => {
+    try {
+      let personId = req.params.id;
+      const cart = await model.findAll(cartModel, {
         where: {
           customerId: personId,
         },
-      })
-      .then((cart) => {
-        let total = 0;
-        let promises = [];
-
-        cart.forEach((cartItem) => {
-          let itemId = cartItem.itemIds;
-          let promise = model
-            .findOne(itemModel, {
-              where: {
-                itemId: itemId,
-              },
-            })
-            .then((item) => {
-              total = total + item.price;
-            });
-          promises.push(promise);
-        });
-
-        Promise.all(promises).then(() => {
-          res.send(total.toString());
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(400).send();
       });
+      let total = 0;
+      await Promise.all(
+        cart.map(async (cartItem) => {
+          const item = await model.findOne(itemModel, {
+            where: {
+              itemId: cartItem.itemIds,
+            },
+          });
+          total += item.price;
+        })
+      );
+
+      res.json({
+        status: "success",
+        data: total.toString(),
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: error,
+      });
+    }
   },
 
   //--> add items to cart list <--
-  add: (req, res, next) => {
-    let body = req.body;
+  add: async (req, res, next) => {
+    try {
+      let body = req.body;
 
-    model.create(cartModel, body).then(
-      (create) => {
-        res.send(create);
-      },
-      (err) => {
-        res.status(400).send({
-          error: "User can not found",
-        });
-      }
-    );
+      var created = await model.create(cartModel, body);
+      res.json({
+        status: "success",
+        data: created,
+      });
+    } catch (error) {
+      res.status(400).send({
+        error: "User can not found",
+      });
+    }
   },
 
   //--> update a item in cart <--
   update: async (req, res, next) => {
-    let cartId = req.params.id;
-    let body = req.body;
-    let condition = {
-      where: {
-        id: cartId,
-      },
-    };
+    try {
+      let cartId = req.params.id;
+      let body = req.body;
+      let condition = {
+        where: {
+          id: cartId,
+        },
+      };
 
-    const foundCart = await model.findOne(cartModel, condition);
+      const foundCart = await model.findOne(cartModel, condition);
 
-    var updatedCart = await model.update(foundCart, body);
-    res.status(400).send(updatedCart);
+      var updatedCart = await model.update(foundCart, body);
+      res.json({
+        status: "success",
+        data: updatedCart,
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: error,
+      });
+    }
   },
 
   //--> delete a item in cart <--
-  deleteOne: (req, res, next) => {
-    let cartID = req.params.id;
-    model
-      .delete(cartModel, {
+  deleteOne: async (req, res, next) => {
+    try {
+      let cartID = req.params.id;
+      var rowdeleted = await model.delete(cartModel, {
         where: {
           id: cartID,
         },
-      })
-      .then(
-        (rowdeleted) => {
-          if (rowdeleted === 0) {
-            res.status(404).send({
-              error: "Item in cart can not found.",
-            });
-          } else {
-            res.status(204).send();
-          }
-        },
-        () => {
-          res.status(500).send();
-        }
-      );
+      });
+
+      if (rowdeleted === 0) {
+        res.status(404).send({
+          error: "Item in cart can not found.",
+        });
+      } else {
+        res.json({
+          status: "success",
+          data: rowdeleted,
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        error: error,
+      });
+    }
   },
 
   //delete cart
-  deleteAll: (req, res, next) => {
-    let personId = req.params.id;
-    model
-      .findAll(cartModel, {
+  deleteAll: async (req, res, next) => {
+    try {
+      let personId = req.params.id;
+      const cart = await model.findAll(cartModel, {
         where: {
           customerId: personId,
         },
-      })
-      .then((cart) => {
-        let total = 0;
-        let promises = [];
-
-        cart.forEach((cartItem) => {
-          let itemId = cartItem.id;
-          let promise = model
-            .delete(cartModel, {
-              where: {
-                id: itemId,
-              },
-            })
-            .then((item) => {
-              total = total + 1;
-            });
-          promises.push(promise);
-        });
-
-        Promise.all(promises).then(() => {
-          res.send(total.toString());
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(400).send();
       });
+
+      cart.forEach((cartItem) => {
+        let itemId = cartItem.id;
+        model.delete(cartModel, {
+          where: {
+            id: itemId,
+          },
+        });
+      });
+
+      res.json({
+        status: "success",
+      });
+    } catch (error) {
+      res.status(500).send({
+        error: error,
+      });
+    }
   },
 };
