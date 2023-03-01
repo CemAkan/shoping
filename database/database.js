5542a5877efcc7ed5e190b010e6d0aa35219c293
CemAkan <cemakan5014@gmail.com>
2023-03-01 14:03:22 +0300

    requires edited

--- database/database.js
+++ database/database.js
@@ -1,8 +1,8 @@
 //--> Requirements <--
 var user = require("../models/user/user");
-var cart = require("../models/cart/cart");
-var category = require("../models/category/category");
-var like = require("../models/likeList/likeList");
+var cart = require("../models/user/cart");
+var category = require("../models/items/category");
+var like = require("../models/user/likeList");
 var item = require("../models/items/item");
 var Database = require("../config/config");
 
