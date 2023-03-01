e9e28d21719bc816158e189cf554327b9b1554db
CemAkan <cemakan5014@gmail.com>
2023-03-01 15:35:53 +0300

    exporting value changed

--- controllers/categoryControler.js
+++ controllers/categoryControler.js
@@ -6,71 +6,69 @@ const checkAuth = require("../middleware/middleware");
 const model = require("../services/modelService");
 
 // export variable
-var Category = {};
+module.exports = {
+  //--> List all categories <--
+  list: async (req, res, next) => {
+    await model.findAll(categoryModel).then((categories) => {
+      res.json(categories);
+    });
+  },
 
-//--> List all categories <--
-Category.list = async (req, res, next) => {
-  await model.findAll(categoryModel).then((categories) => {
-    res.json(categories);
-  });
-};
+  //--> Add a category <--
+  add: (req, res, next) => {
+    let body = req.body;
 
-//--> Add a category <--
-Category.add = (req, res, next) => {
-  let body = req.body;
+    model.create(categoryModel, body).then(
+      (category) => {
+        res.json(category);
+      },
+      (err) => {
+        res.status(400).send({
+          error: "Please use correct writing rules.",
+        });
+      }
+    );
+  },
 
-  model.create(categoryModel, body).then(
-    (category) => {
-      res.json(category);
-    },
-    (err) => {
-      res.status(400).send({
-        error: "Please use correct writing rules.",
-      });
-    }
-  );
-};
+  //--> Update a category <--
+  update: async (req, res, next) => {
+    let category_Id = req.params.id;
+    let body = req.body;
 
-//--> Update a category <--
-Category.update = async (req, res, next) => {
-  let category_Id = req.params.id;
-  let body = req.body;
-
-  let condition = {
-    where: {
-      categoryId: category_Id,
-    },
-  };
-
-  const foundCategory = await model.findOne(categoryModel, condition);
-
-  var updatedCategory = await model.update(foundCategory, body);
-  res.status(400).send(updatedCategory);
-};
-
-//--> Delete a category <--
-Category.delete = (req, res, next) => {
-  let category_Id = req.params.id;
-  model
-    .delete(categoryModel, {
+    let condition = {
       where: {
         categoryId: category_Id,
       },
-    })
-    .then(
-      (rowdeleted) => {
-        if (rowdeleted === 0) {
-          res.status(404).send({
-            error: "Item can not found.",
-          });
-        } else {
-          res.status(204).send();
-        }
-      },
-      () => {
-        res.status(500).send();
-      }
-    );
-};
+    };
 
-module.exports = Category;
+    const foundCategory = await model.findOne(categoryModel, condition);
+
+    var updatedCategory = await model.update(foundCategory, body);
+    res.status(400).send(updatedCategory);
+  },
+
+  //--> Delete a category <--
+  deleting: (req, res, next) => {
+    let category_Id = req.params.id;
+    model
+      .delete(categoryModel, {
+        where: {
+          categoryId: category_Id,
+        },
+      })
+      .then(
+        (rowdeleted) => {
+          if (rowdeleted === 0) {
+            res.status(404).send({
+              error: "Item can not found.",
+            });
+          } else {
+            res.status(204).send();
+          }
+        },
+        () => {
+          res.status(500).send();
+        }
+      );
+  },
+};
