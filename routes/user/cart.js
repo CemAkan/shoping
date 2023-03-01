29e12777a03a2410e2125e82bd2d740fa31a3211
CemAkan <cemakan5014@gmail.com>
2023-03-01 15:27:57 +0300

    require for controller edited

--- routes/user/cart.js
+++ routes/user/cart.js
@@ -1,27 +1,34 @@
 //--> Module dependencies <--
 var express = require("express");
 var router = express.Router();
-var Carts = require("../../controllers/cartControler");
+var {
+  deleteAll,
+  deleteOne,
+  update,
+  add,
+  list,
+  price,
+} = require("../../controllers/cartControler");
 
 //--> routes for cart <--
 
 //--> list all items that were added to cart <--
-router.get("/list/:id", Carts.list); //id = customerId
+router.get("/list/:id", list); //id = customerId
 
 //--> get total price of all items that were added to cart <--
-router.get("/price/:id", Carts.price); //id = customerId
+router.get("/price/:id", price); //id = customerId
 
 //--> add items to cart <--
-router.post("/add/:id", Carts.add); //id = customerId
+router.post("/add/:id", add); //id = customerId
 
 //--> update a item in cart <--
-router.put("/update/:id", Carts.update); //id = id
+router.put("/update/:id", update); //id = id
 
 //--> delete a item in cart <--
-router.delete("/delete-one/:id", Carts.deleteOne); //id = id
+router.delete("/delete-one/:id", deleteOne); //id = id
 
 //--> delete cart <--
-router.delete("/delete-all/:id", Carts.deleteAll); //id = customerId
+router.delete("/delete-all/:id", deleteAll); //id = customerId
 
 //exporting
 module.exports = router;
