de4e87669e838730381637713d46b0ce3485a0c3
CemAkan <cemakan5014@gmail.com>
2023-03-02 23:33:59 +0300

    pk and fk removed

--- models/items/item.js
+++ models/items/item.js
@@ -1,16 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
   return sequelize.define("item", {
-    //primary key
-    itemId: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNUll: false,
-      unique: true,
-      validate: {
-        len: [1, 10],
-      },
-    },
     //name of item
     name: {
       type: Sequelize.STRING,
