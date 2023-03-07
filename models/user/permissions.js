b78650440a2b986c2b3ceb9043f7097515c4aed3
CemAkan <cemakan5014@gmail.com>
2023-03-07 05:07:16 +0300

    default value set false for permissions

--- models/user/permissions.js
+++ models/user/permissions.js
@@ -2,9 +2,11 @@ module.exports = (sequelize, Sequelize) => {
   return sequelize.define("permission", {
     SMS: {
       type: Sequelize.BOOLEAN,
+      defaultValue: false,
     },
     email: {
       type: Sequelize.BOOLEAN,
+      defaultValue: false,
     },
   });
 };
