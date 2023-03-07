98533d036af589d24f8ee9cd4bbc6e83d28a56a5
CemAkan <cemakan5014@gmail.com>
2023-03-07 04:36:06 +0300

    new models import and make active

--- database/database.js
+++ database/database.js
@@ -1,10 +1,26 @@
 //--> Requirements <--
+
+//variables for database connection
+var Database = require("../config/config");
+
+//user models
 var user = require("../models/user/user");
 var cart = require("../models/user/cart");
-var category = require("../models/items/category");
 var like = require("../models/user/likeList");
+var address = require("../models/user/address");
+var permissions = require("../models/user/permissions");
+var card = require("../models/user/card");
+
+//item models
 var item = require("../models/items/item");
-var Database = require("../config/config");
+var category = require("../models/items/category");
+var announcements = require("../models/items/announcements");
+var itemVariants = require("../models/items/itemVariants");
+var similarItems = require("../models/items/similarItems");
+
+//delivery models
+var deliveryDetails = require("../models/delivery/deliveryDetails");
+var oldOrders = require("../models/delivery/oldOrders");
 
 //--> Database Connection <--
 const Sequelize = require("sequelize");
@@ -19,25 +35,39 @@ const sequelize = new Sequelize(
 );
 
 //--> Sequelize to Models <--
+
+//user models
 const userModel = user(sequelize, Sequelize);
 const cartModel = cart(sequelize, Sequelize);
 const categoryModel = category(sequelize, Sequelize);
 const likeModel = like(sequelize, Sequelize);
+const permissionsModel = permissions(sequelize, Sequelize);
+const cardModel = card(sequelize, Sequelize);
+const addressModel = address(sequelize, Sequelize);
+
+//item models
 const itemModel = item(sequelize, Sequelize);
+const announcementsModel = announcements(sequelize, Sequelize);
+const itemVariantsModel = itemVariants(sequelize, Sequelize);
+const similarItemsModel = similarItems(sequelize, Sequelize);
+
+//delivery models
+const deliveryDetailsModel = deliveryDetails(sequelize, Sequelize);
+const oldOrdersModel = oldOrders(sequelize, Sequelize);
 
 //--> Associations <--
 
 // CART <-> USER
-userModel.hasMany(cartModel, { foreignKey: "userId" });
-cartModel.belongsTo(userModel, { foreignKey: "userId" });
+userModel.hasMany(cartModel, { foreignKey: "phone" });
+cartModel.belongsTo(userModel, { foreignKey: "phone" });
 
 // ITEM <-> CATEGORY
 categoryModel.hasMany(itemModel, { foreignKey: "category_Id" });
 itemModel.belongsTo(categoryModel, { foreignKey: "category_Id" });
 
 // LIKE <-> USER
-userModel.hasMany(likeModel, { foreignKey: "userId" });
-likeModel.belongsTo(userModel, { foreignKey: "userId" });
+userModel.hasMany(likeModel, { foreignKey: "phone" });
+likeModel.belongsTo(userModel, { foreignKey: "phone" });
 
 // LIKE <-> ITEM
 itemModel.hasMany(likeModel, { foreignKey: "itemId" });
@@ -47,10 +77,18 @@ likeModel.belongsTo(itemModel, { foreignKey: "itemId" });
 itemModel.hasMany(cartModel, { foreignKey: "itemId" });
 cartModel.belongsTo(itemModel, { foreignKey: "itemId" });
 
-//--> export <--
+//--> exporting variables <--
 module.exports = {
   Sequelize,
   sequelize,
+  permissionsModel,
+  announcementsModel,
+  itemVariantsModel,
+  similarItemsModel,
+  cardModel,
+  deliveryDetailsModel,
+  oldOrdersModel,
+  addressModel,
   userModel,
   cartModel,
   categoryModel,
