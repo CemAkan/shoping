d85246f03b64b8d8db15dda20a24ce3197b0ab40
CemAkan <cemakan5014@gmail.com>
2023-03-07 04:31:15 +0300

    phone and gender column added

--- models/user/user.js
+++ models/user/user.js
@@ -2,6 +2,12 @@ const { userRoleTypes } = require("../../helpers/roleTypes");
 
 module.exports = (sequelize, Sequelize) => {
   return sequelize.define("user", {
+    //user's phone number for primary key
+    phone: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    //user's username
     username: {
       type: Sequelize.STRING,
       allowNUll: false,
@@ -9,6 +15,7 @@ module.exports = (sequelize, Sequelize) => {
         len: [1, 10],
       },
     },
+    //user's email address
     email: {
       type: Sequelize.STRING,
       allowNUll: false,
@@ -17,22 +24,31 @@ module.exports = (sequelize, Sequelize) => {
         isEmail: true,
       },
     },
+    //user's password
     password: {
       type: Sequelize.STRING(64),
       validate: {
         is: /^[0-9a-f]{64}$/i,
       },
     },
+    //user's role
     role: {
       type: Sequelize.STRING,
       defaultValue: userRoleTypes.user,
     },
+    //access token for accessing toadmin routes
     access_token: {
       type: Sequelize.STRING,
     },
+    //activity status for user
     account_activity_status: {
       type: Sequelize.BOOLEAN,
       defaultValue: true,
+    },
+    //user's gender
+    gender: {
+      type: Sequelize.ENUM("M", "F", "Other"),
+      allowNull: false,
     },
   });
 };
