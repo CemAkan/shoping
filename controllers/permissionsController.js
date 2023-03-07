8e12ff6bf4c5a675e326a404c6f149e40cff9c88
CemAkan <cemakan5014@gmail.com>
2023-03-07 06:28:04 +0300

    permission controller added

---
+++ controllers/permissionsController.js
@@ -0,0 +1,50 @@
+//--> Module dependencies.<--
+var express = require("express");
+var router = express.Router();
+var { permissionsModel } = require("../database/database");
+const model = require("../services/modelService");
+
+// export variable
+module.exports = {
+  //--> List all permissions <--
+  listAllPermissions: async (req, res, next) => {
+    try {
+      await model.findAll(permissionsModel).then((permissions) => {
+        res.json({ status: "success", data: permissions });
+      });
+    } catch (error) {
+      res.json({
+        status: "error",
+        error: error,
+      });
+    }
+  },
+
+  //--> Update a permissions <--
+  updatePermissions: async (req, res, next) => {
+    try {
+      let ID = req.params.id;
+      let body = req.body;
+      let condition = {
+        where: {
+          id: ID,
+        },
+      };
+
+      var updatedpermissions = await model.update(
+        permissionsModel,
+        body,
+        condition
+      );
+      res.json({
+        status: "success",
+        data: updatedpermissions,
+      });
+    } catch (error) {
+      res.json({
+        status: "error",
+        error: error,
+      });
+    }
+  },
+};
