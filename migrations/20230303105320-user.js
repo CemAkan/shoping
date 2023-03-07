560d6aac4440ccb1dab8cfc1b80be6df755c554e
CemAkan <cemakan5014@gmail.com>
2023-03-07 04:29:11 +0300

    id and userId change with phone

--- migrations/20230303105320-user.js
+++ migrations/20230303105320-user.js
@@ -2,18 +2,85 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    //new id create after that customerId infos copied to id column
+    //new phone create after that id infos copied to phone column
 
-    await queryInterface.addColumn("users", "id", {
+    await queryInterface.addColumn("users", "phone", {
       type: Sequelize.INTEGER,
     });
 
     await queryInterface.sequelize.query(`
       UPDATE users
-      SET id = customerId
+      SET phone = id
     `);
 
-    //create userId and after that customerId infos copied to userId column
+    //create phone and after that userId infos copied to phone column
+
+    await queryInterface.addColumn("likes", "phone", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    });
+
+    await queryInterface.sequelize.query(`
+      UPDATE likes
+      SET phone = userId
+    `);
+
+    //create phone and after that userId infos copied to phone column
+
+    await queryInterface.addColumn("carts", "phone", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    });
+
+    await queryInterface.sequelize.query(`
+      UPDATE carts
+      SET phone = userId
+    `);
+
+    //old columns remove
+
+    await queryInterface.removeColumn("likes", "userId");
+    await queryInterface.removeColumn("carts", "userId");
+    await queryInterface.removeColumn("users", "id");
+
+    //new colums' properties change
+    await queryInterface.changeColumn("users", "phone", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    });
+    await queryInterface.changeColumn("carts", "phone", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "phone",
+      },
+    });
+    await queryInterface.changeColumn("likes", "phone", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "phone",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    //userId create after that phone infos copied to id column
+
+    await queryInterface.addColumn("users", "userId", {
+      type: Sequelize.INTEGER,
+    });
+
+    await queryInterface.sequelize.query(`
+      UPDATE users
+      SET id = phone
+    `);
+
+    //create userId and after that phone infos copied to userId column
 
     await queryInterface.addColumn("likes", "userId", {
       type: Sequelize.INTEGER,
@@ -22,10 +89,10 @@ module.exports = {
 
     await queryInterface.sequelize.query(`
       UPDATE likes
-      SET userId = customerId
+      SET userId = phone
     `);
 
-    //create userId and after that customerId infos copied to userId column
+    //create userId and after that phone infos copied to userId column
 
     await queryInterface.addColumn("carts", "userId", {
       type: Sequelize.INTEGER,
@@ -34,17 +101,17 @@ module.exports = {
 
     await queryInterface.sequelize.query(`
       UPDATE carts
-      SET userId = customerId
+      SET userId = phone
     `);
 
     //old columns remove
 
-    await queryInterface.removeColumn("likes", "customerId");
-    await queryInterface.removeColumn("carts", "customerId");
-    await queryInterface.removeColumn("users", "customerId");
+    await queryInterface.removeColumn("likes", "phone");
+    await queryInterface.removeColumn("carts", "phone");
+    await queryInterface.removeColumn("users", "phone");
 
     //new colums' properties change
-    await queryInterface.changeColumn("users", "id", {
+    await queryInterface.changeColumn("users", "userId", {
       type: Sequelize.INTEGER,
       allowNull: false,
       autoIncrement: true,
@@ -55,7 +122,7 @@ module.exports = {
       allowNull: false,
       references: {
         model: "users",
-        key: "id",
+        key: "phone",
       },
     });
     await queryInterface.changeColumn("likes", "userId", {
@@ -63,74 +130,7 @@ module.exports = {
       allowNull: false,
       references: {
         model: "users",
-        key: "id",
-      },
-    });
-  },
-
-  async down(queryInterface, Sequelize) {
-    //customerId create after that id infos copied to customerId column
-
-    await queryInterface.addColumn("users", "customerId", {
-      type: Sequelize.INTEGER,
-    });
-
-    await queryInterface.sequelize.query(`
-      UPDATE users
-      SET customerId = id
-    `);
-
-    //create customerId and after that userId infos copied to customerId column
-
-    await queryInterface.addColumn("likes", "customerId", {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    });
-
-    await queryInterface.sequelize.query(`
-      UPDATE likes
-      SET customerId = userId
-    `);
-
-    //create customerId and after that userId infos copied to customerId column
-
-    await queryInterface.addColumn("carts", "customerId", {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-    });
-
-    await queryInterface.sequelize.query(`
-      UPDATE carts
-      SET customerId = userId
-    `);
-
-    //old columns remove
-
-    await queryInterface.removeColumn("likes", "userId");
-    await queryInterface.removeColumn("carts", "userId");
-    await queryInterface.removeColumn("users", "id");
-
-    //new colums' properties change
-    await queryInterface.changeColumn("users", "customerId", {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-    });
-    await queryInterface.changeColumn("carts", "customerId", {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references: {
-        model: "users",
-        key: "id",
-      },
-    });
-    await queryInterface.changeColumn("likes", "customerId", {
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references: {
-        model: "users",
-        key: "id",
+        key: "phone",
       },
     });
   },
