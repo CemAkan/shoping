77e56e325b99301d5306fc1dad8b51b789df1100
Cem Akan <90530863+CemAkan@users.noreply.github.com>
2023-03-09 13:33:50 +0300

    photo upload to cloud

---
+++ services/photoUpload.js
@@ -0,0 +1,71 @@
+// Require modules
+const aws = require("aws-sdk");
+const express = require("express");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+require("dotenv").config();
+
+const app = express();
+
+const spacesEndpoint = new aws.Endpoint("fra1.digitaloceanspaces.com");
+const s3 = new aws.S3({
+  endpoint: spacesEndpoint,
+  accessKeyId: process.env.DO_ACCESS_KEY,
+  secretAccessKey: process.env.DO_SECRET_ACCESS_KEY,
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(new Error("Invalid file type"), false);
+  }
+};
+
+let turkishLetters = {
+  ı: "i",
+  ö: "o",
+  ğ: "g",
+  ç: "c",
+  ü: "u",
+  ş: "s",
+  İ: "I",
+  Ö: "O",
+  Ğ: "G",
+  Ç: "C",
+  Ü: "U",
+  Ş: "S",
+  " ": "-",
+};
+
+const checkTurkishLetters = (letter) => {
+  if (turkishLetters[letter]) {
+    return turkishLetters[letter];
+  } else return letter;
+};
+
+const fileName = (originalname) => {
+  let convertedName = "";
+  for (let i = 0; i < originalname.length; i++) {
+    let letter = checkTurkishLetters(originalname[i]);
+    convertedName += letter;
+  }
+  return convertedName;
+};
+
+const uploadDO = multer({
+  fileFilter,
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.DO_BUCKET_NAME,
+    acl: "public-read",
+    key: function (request, file, cb) {
+      let convertedName = Date.now() + "-" + fileName(file.originalname);
+      var fullPath = "images/" + convertedName;
+      console.log(file);
+      cb(null, fullPath);
+    },
+  }),
+});
+
+module.exports = uploadDO;
