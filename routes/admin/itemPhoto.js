4f1521950bbe4c10f33929d6e9a0bcd6b5b69445
Cem Akan <90530863+CemAkan@users.noreply.github.com>
2023-03-09 13:46:15 +0300

    add and update controller methods linked with get and post request

--- routes/admin/itemPhoto.js
+++ routes/admin/itemPhoto.js
@@ -1,10 +1,15 @@
 //--> Module dependencies.<--
 var express = require("express");
 var router = express.Router();
+var {
+  addPhoto,
+  deletePhoto,
+} = require("../../controllers/itemPhotoController");
 
 //--> Add a photo <--
-router.post("/add", uploadDO.array("file", 10), (req, res) => {
-  res.send("Files successfuly upload!");
-});
+router.post("/add", addPhoto);
+
+//--> Delete a photo <--
+router.delete("/delete/:id", deletePhoto); //photo id
 
 module.exports = router;
