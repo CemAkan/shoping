23fde4077cd1254c74a14a5cb90555465e89a853
Cem Akan <90530863+CemAkan@users.noreply.github.com>
2023-03-10 17:29:46 +0300

    photo upload to drive addded also body parser added

--- routes/admin/announcements.js
+++ routes/admin/announcements.js
@@ -1,6 +1,15 @@
 //--> Module dependencies.<--
 var express = require("express");
+var app = express();
 var router = express.Router();
+var uploadImageDO = require("../../services/photoUpload");
+
+//body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//controllers
 var {
   listAllAnnouncement,
   deleteAnnouncement,
@@ -8,11 +17,13 @@ var {
   updateAnnouncement,
 } = require("../../controllers/announcementController");
 
+//Requests
+
 //--> List all announcements <--
 router.get("/list", listAllAnnouncement);
 
 //--> Add a announcement <--
-router.post("/add", addAnnouncement);
+router.post("/add", uploadImageDO.array("photo", 5), addAnnouncement);
 
 //--> Update a announcement <--
 router.put("/update/:id", updateAnnouncement);
--- routes/admin/items.js
+++ routes/admin/items.js
@@ -1,6 +1,15 @@
 //--> Module dependencies.<--
 var express = require("express");
+var app = express();
 var router = express.Router();
+var uploadImageDO = require("../../services/photoUpload");
+
+//body-parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//controllers
 var {
   addItem,
   listAllItem,
@@ -8,11 +17,13 @@ var {
   deleteItem,
 } = require("../../controllers/itemControler");
 
+//Requests
+
 //--> List all items <--
 router.get("/list", listAllItem);
 
 //--> Add a item <--
-router.post("/add", addItem);
+router.post("/add", uploadImageDO.array("photo", 5), addItem);
 
 //--> Update a item <--
 router.put("/update/:id", updateItem);
