25d9ec8ec8c8c51685b6226d53029424f24ea49d
Cem Akan <90530863+CemAkan@users.noreply.github.com>
2023-03-10 04:18:24 +0300

    for all photos' method controllers  merge at one controller file instead of two different file

---
+++ controllers/photoController.js
@@ -0,0 +1,108 @@
+//--> Module dependencies.<--
+var express = require("express");
+var router = express.Router();
+var { photoModel, sequelize } = require("../database/database");
+const model = require("../services/modelService");
+
+// export variable
+module.exports = {
+  addPhoto: async (req, res, next) => {
+    const t = await sequelize.transaction();
+    var counter = 0;
+    try {
+      for (const photo of req.files) {
+        if (!req.body.itemId) {
+          req.body.itemId = null;
+        }
+        if (!req.body.announcementId) {
+          req.body.req.body.announcementId = null;
+        }
+
+        await model.findOrCreate(photoModel, {
+          where: {
+            photoLink: photo.location,
+            photoType: photo.mimetype,
+            photoSize: photo.size,
+            announcementId: req.body.announcementId,
+            itemId: req.body.itemId,
+          },
+          defaults: {
+            photoLink: photo.location,
+            photoType: photo.mimetype,
+            photoSize: photo.size,
+            announcementId: req.body.announcementId,
+            itemId: req.body.itemId,
+          },
+        });
+        counter++;
+      }
+
+      await t.commit();
+
+      res.json({
+        status: "success",
+        data: counter + " new photos were added.",
+      });
+    } catch (error) {
+      res.status(422).send({ status: "Error", data: error.message });
+      await t.rollback();
+      next(error);
+    }
+  },
+
+  deletePhoto: async (req, res, next) => {
+    const photoId = req.params.id;
+    try {
+      const deletedPhoto = await model.delete(photoModel, {
+        where: { id: photoId },
+      });
+
+      res.json({ status: "success", data: deletedPhoto });
+    } catch (error) {
+      res.status(500).json({ status: "error", data: error });
+      next(error);
+    }
+  },
+  listPhotoAnnouncement: async (req, res, next) => {
+    try {
+      let ID = req.params.id;
+
+      let condition = {
+        where: {
+          announcementId: ID,
+        },
+      };
+      var photo = await model.findOne(photoModel, condition);
+      res.json({
+        status: "success",
+        data: photo,
+      });
+    } catch (error) {
+      res.json({
+        status: "error",
+        error: error,
+      });
+    }
+  },
+  listPhotoItem: async (req, res, next) => {
+    try {
+      let ID = req.params.id;
+
+      let condition = {
+        where: {
+          id: ID,
+        },
+      };
+      var photo = await model.findOne(photoModel, condition);
+      res.json({
+        status: "success",
+        data: photo,
+      });
+    } catch (error) {
+      res.json({
+        status: "error",
+        error: error,
+      });
+    }
+  },
+};
